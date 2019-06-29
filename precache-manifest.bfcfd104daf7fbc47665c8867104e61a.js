6b0e28a38592e397b11aaf03a05582146a8c48ca
Gerwin Jonathan <gerwinjh12345@gmail.com>
2019-06-29 22:42:54 +0700

    Updates

---
+++ precache-manifest.bfcfd104daf7fbc47665c8867104e61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e185752d609646786603bcea111fef51",
+    "url": "/admin-ui/index.html"
+  },
+  {
+    "revision": "f50ab8c274bf046d9483",
+    "url": "/admin-ui/static/css/main.f372bc7a.chunk.css"
+  },
+  {
+    "revision": "224481a799ea536cb88c",
+    "url": "/admin-ui/static/js/2.22faef3f.chunk.js"
+  },
+  {
+    "revision": "f50ab8c274bf046d9483",
+    "url": "/admin-ui/static/js/main.85675402.chunk.js"
+  },
+  {
+    "revision": "566843bbe7ccb050da71",
+    "url": "/admin-ui/static/js/runtime~main.0b550878.js"
+  }
+]);
